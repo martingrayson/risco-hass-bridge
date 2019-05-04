4ee14513acb1c6e03ffe36bd1cefbbd0a416764f
Martin Grayson <martin@zeddy.org>
2019-05-04 16:53:46 +0100

    changing mqtt config

--- run.sh
+++ run.sh
@@ -3,20 +3,26 @@ set -e
 
 APP_ENTRYPOINT=risco-bridge/main.py
 CONFIG_PATH=/data/options.json
-MQTT_CONFIG=
+#MQTT_CONFIG=
 
 # Use Hass.io mqtt services
-if MQTT_CONFIG="$(curl -s -f -H "X-Hassio-Key: ${HASSIO_TOKEN}" http://hassio/services/mqtt)"; then
-    HOST="$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.host')"
-    PORT="$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.port')"
-    USER="$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.username')"
-    PASSWORD="$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.password')"
+#if MQTT_CONFIG="$(curl -s -f -H "X-Hassio-Key: ${HASSIO_TOKEN}" http://hassio/services/mqtt)"; then
+#    HOST="$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.host')"
+#    PORT="$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.port')"
+#    USER="$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.username')"
+#    PASSWORD="$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.password')"
+#
+#    echo "[INFO] Setup Hass.io mqtt service to ${HOST}"
+#else
+#    echo "[ERROR] No Hass.io mqtt service found!"
+#    exit 1
+#fi
 
-    echo "[INFO] Setup Hass.io mqtt service to ${HOST}"
-else
-    echo "[ERROR] No Hass.io mqtt service found!"
-    exit 1
-fi
+
+HOST=$(jq --raw-output ".mqtt.host" $CONFIG_PATH)
+PORT=$(jq --raw-output ".mqtt.port" $CONFIG_PATH)
+USER=$(jq --raw-output ".mqtt.username" $CONFIG_PATH)
+PASSWORD=$(jq --raw-output ".mqtt.password" $CONFIG_PATH)
 
 RISCO_USER=$(jq --raw-output ".risco.username" $CONFIG_PATH)
 RISCO_PASSWORD=$(jq --raw-output ".risco.password" $CONFIG_PATH)
